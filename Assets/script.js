340c6d1888f494fe4c2f75f6a129ca63db399e25
Mariel Miscavage <memiscavage@gmail.com>
2024-01-30 15:11:01 -0600

    added event listener in JS and updated src in HTML to link to the CSS and JS

--- Assets/script.js
+++ Assets/script.js
@@ -4,7 +4,10 @@
 $(function () {
   // TODO: Add a listener for click events on the save button.
   // This code should use the id in the containing time-block as a key to save the user input in local storage.
-  // HINT: What does `this` reference in the click listener function?
+  // HINT: What does `this` reference in the click listener function? 
+  $( "button" ).on( "click", function() {
+    $( this ).save();
+  } );
   // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
   // How might the id be useful when saving the description in local storage?
   //
